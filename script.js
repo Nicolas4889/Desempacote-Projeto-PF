7420ce0e275ae322c480dfb7e018a85bb4a04e6c
DavissonC <davisson.costa@dcomp.ufs.br>
2025-08-15 23:55:27 -0300

    Adição da função porcentagem
    A função porcentagem mostra a taxa de sucesso do jogador no level, caso ele não acerte tudo perfeitamente

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ const organizador = (fase, jogador) => {
         return objetoEsperado === jogador[indice];
     })
 }
+//Cria uma função que calcula a porcentagem de acerto do jogador, isto é, mede o quão próximo o jogador chegou de acertar a posição dos objetos.
+//Essa função será implementada na função comparador
+const porcentagem = (fase, jogador) => (jogador/fase)*100
 //Cria uma função responsável por analisar se o tamanho da lista da fase é igual ao tamanho da lista filtrada pela função organizadora, e então retorna se sim ou não.
 
 const comparador = (fase, jogador) => (a, ...b) => {
     jogador = [a,...b]
     const resultadoOrganizador = organizador(fase,jogador)
+    resultadoPorcentagem= porcentagem(fase.length, resultadoOrganizador.length)
     if (resultadoOrganizador.length === fase.length){
         return "Parabéns, você completou a  fase!"
     }
-    else {return 'Tente Novamente!'}
+    else {return `Tente Novamente! Você está ${resultadoPorcentagem}% correto` }
 }
 //Ou seja: As duas funções basicamente verificam se a lista organizada pelo jogador é igual à pensada pelos desenvolvedores para a fase.
 
